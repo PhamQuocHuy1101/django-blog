534af82dd00c30053eda24f8d05cdc2c16820633
huypq <huyphamquoc@admicro.vn>
2022-08-10 10:56:22 +0700

    add config

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
+print("settings: ", settings.TIME_ZONE)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
